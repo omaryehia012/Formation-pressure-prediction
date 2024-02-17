92736a7543f79a950e9d5077b7b3582eb271e96d
Omar Mohamed Yehia <93586279+omaryehia012@users.noreply.github.com>
2024-02-17 09:10:52 +0200

    Update kick_detection_app_All.py

--- kick_detection_app_All.py
+++ kick_detection_app_All.py
@@ -3,13 +3,17 @@ import joblib
 import pandas as pd
 import xgboost
 
+# Load pre-trained scaler and model
 scaler = joblib.load("models/scaler_All.h5")
 Model = joblib.load("models/model_All.h5")
-Inputs=['TVD(ft)', 'BITSIZE(in)', 'NPHI(%)',
-       'Corrected Bulk Density(gm/cc)', 'Deep Resistivity (Ohm)', 'ROP(M/hr)',
-       'WOB(KLb)', 'RPM', 'Torque(lb.F)', 'Stand Pipe Pressure(Psi)',
-       'Flow In(GPM)','Temp - Out', 'Total Gas(PPM)']
 
+# Define input features
+Inputs=['TVD(ft)', 'BITSIZE(in)', 'NPHI(%)',
+        'Corrected Bulk Density(gm/cc)', 'Deep Resistivity (Ohm)', 'ROP(M/hr)',
+        'WOB(KLb)', 'RPM', 'Torque(lb.F)', 'Stand Pipe Pressure(Psi)',
+        'Flow In(GPM)','Temp - Out', 'Total Gas(PPM)']
+
+# Function to make prediction
 def predict(TVD_FT,BITSIZE,NPHI,Corrected_Bulk_Density,Deep_Resistivity ,ROP,WOB,RPM,Torque,Stand_Pipe_Pressure,
                   Flow_In,Temp_Out,Total_Gas):
     test_df = pd.DataFrame(columns = Inputs,index=[0])
@@ -24,21 +28,20 @@ def predict(TVD_FT,BITSIZE,NPHI,Corrected_Bulk_Density,Deep_Resistivity ,ROP,WOB
     test_df.at[0,"Torque(lb.F)"] = Torque
     test_df.at[0,"Stand Pipe Pressure(Psi)"] = Stand_Pipe_Pressure
     test_df.at[0,"Flow In(GPM)"] = Flow_In
-    #test_df.at[0,"Temp - In"] = Temp_In
     test_df.at[0,"Temp - Out"] = Temp_Out
     test_df.at[0,"Total Gas(PPM)"] = Total_Gas
 
     result = Model.predict(scaler.transform(test_df))[0]
     return result
-    
 
+# Function to style header
 def header(url):
      st.markdown(f'<p style="background-color:#0066cc;color:#33ff33;font-size:24px;border-radius:2%;">{url}</p>', unsafe_allow_html=True)    
 
-# this is the main function in which we define our webpage  
+# Main function to define the app layout and functionality
 def main():       
-     # Front-end elements of the web page 
-    html_temp = """
+    # Front-end elements of the web page 
+    st.markdown("""
     <style>
         body {
             background-image: url('https://pluspng.com/img-png/oil-rig-png-hd--2880.jpg');
@@ -64,17 +67,12 @@ def main():
             color: black; /* Adjust the text color as needed */
         }
     </style>
-
     <div class="header">
         <h1>Kick Detection System ML Prediction App</h1>
     </div>
-
-    """
+    """, unsafe_allow_html=True)
     
-    # Display the front-end aspect
-    st.markdown(html_temp, unsafe_allow_html=True)
-    
-    # Following lines create boxes in which the user can enter data required to make a prediction
+    # Sidebar for user input
     st.sidebar.title("Choose your Features")
     TVD_FT = st.sidebar.number_input('TVD(ft)', min_value=0, max_value=100000, value=0, step=1)
     BITSIZE = st.sidebar.number_input('BITSIZE(in)')
@@ -87,21 +85,19 @@ def main():
     Torque = st.sidebar.number_input('Torque(lb.F)')
     Stand_Pipe_Pressure = st.sidebar.number_input('Stand Pipe Pressure(Psi)')
     Flow_In = st.sidebar.number_input('Flow In(GPM)')
-    #Temp_In = st.sidebar.number_input('Temp - In')
     Temp_Out = st.sidebar.number_input('Temp - Out')
     Total_Gas = st.sidebar.number_input('Total Gas(PPM)')
-    result = ""
-          
-    # When 'Predict' is clicked, make the prediction and store it 
+    
+    # Prediction button
     if st.sidebar.button("Predict"): 
         result = predict(TVD_FT,BITSIZE,NPHI,Corrected_Bulk_Density,Deep_Resistivity ,ROP,WOB,RPM,Torque,Stand_Pipe_Pressure,
                   Flow_In,Temp_Out,Total_Gas)
-                
-        ## Print final Prediction 
+        
+        # Display result
         st.markdown(f'<h1 style="color:#33ff33;font-size:40px;text-align:center;border-style: solid;border-width:5px;border-color:#fbff00;">{result}</h1>', unsafe_allow_html=True)
    
-    # Show restaurant image
-    st.image('R.jfif', use_column_width=True)  # Adjust 'use_column_width' as needed
+    # Show image
+    st.image('R.jfif', use_column_width=True) 
     
 if __name__=='__main__': 
     main()
