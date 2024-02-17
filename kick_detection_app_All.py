60b746a72c4cac38bd398932b33d60cf34b3a7b0
Omar Mohamed Yehia <93586279+omaryehia012@users.noreply.github.com>
2024-02-17 09:59:50 +0200

    Update kick_detection_app_All.py

--- kick_detection_app_All.py
+++ kick_detection_app_All.py
@@ -118,7 +118,7 @@ def main():
         
         # Display result
         result_display = f"""
-        <div style="background-color: rgba(6,71,186,0.3); padding:2px; border-radius:10px;">
+        <div style="background-color: rgba(6,71,51,0.3); padding:2px; border-radius:10px;">
             <h1 style="color:white; font-size:40px; text-align:center;">Formation Pressure: <span style="color:{color};">{result}</span></h1>
             <h1 style="color:white; font-size:40px; text-align:center;">Hydrostatic Pressure: {PH}</h1>
             <h1 style="color:white; font-size:40px; text-align:center;"><span style="color:{color};">{Pred}</span></h1>
